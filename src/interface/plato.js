22d63355e268cf52f5b35ea43fa9bc0b11e07235
julien-leroy <julien.leroy9@gmail.com>
2015-02-06 09:19:29 +0100

    add plato and create palto interface. ready to test

---
+++ src/interface/plato.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var shjs = require("shelljs");
+var p = require('path');
+
+module.exports = function (options, f) {
+  // @todo do not rely on "/usr/bin/env"
+  var cmd = 'plato -r -d report ' + p.resolve(__dirname);
+  var ret = shjs.exec(cmd).code;
+
+
+  if (ret !== 0) {
+    return f(new Error('Security issue'));
+  }
+
+  var platoRes = shjs.cat(p.resolve(__dirname, '../../node_modules/plato/bin/plato.js package'));
+  var maintainability = parseFloat(JSON.parse(platoRes).average.maintainability);
+
+  if (maintainability < 70) {
+    return f(new Error('Your project average maintainability is under 70%'));
+  }
+
+  f();
+};
