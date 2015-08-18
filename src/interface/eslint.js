5d09f21b457fa5c4a33b819a4d162183d7739474
FG Ribreau <github@fgribreau.com>
2015-08-18 16:08:32 +0200

    feat(eslint): support for ESLint (jsxhint is deprecated), fixes #21

---
+++ src/interface/eslint.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var eslintcli = require('eslint/lib/cli');
+var p = require('path');
+var grunt = require('grunt');
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+module.exports = function (debug) {
+  var utils = require('./_utils')(debug);
+
+  return function (options, f) {
+    options = options || {};
+
+    utils.downloadDistantOrLoad(options.url, onConfigLoaded);
+
+    function onConfigLoaded(err) {
+      if (err) {
+        return f(err);
+      }
+
+      var config = p.resolve(process.cwd(), './.eslintrc');
+      var files = grunt.file.expand(options.args);
+      var args = ['--config', config].concat(files);
+      var NODE_MODULES = p.resolve(__dirname, '../../', 'node_modules/');
+      var eslint = p.resolve(NODE_MODULES, '.bin/eslint');
+
+      var cmd = [eslint].concat(args).join(' ');
+
+      debug('running eslint with %s', cmd);
+
+      ['node', 'check-build'].concat();
+
+      exec(cmd, function (error, stdout, stderr) {
+        console.log(stdout);
+
+        if (stderr) {
+          console.error(stderr);
+        }
+
+        var hadError = error && error.code !== 0;
+
+        if(!hadError){
+          console.log('✔ No problems');
+        }
+
+        f(hadError ? '' : null);
+      });
+    }
+  };
+};
